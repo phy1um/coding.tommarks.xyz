cf63b8c4d5f6ae9ee8e25f5ecc57fb5570914cd8
Tom Marks <tom@tommarks.xyz>
2021-04-25 14:06:30 +1000

    Made blog template file a variable

--- make-site.sh
+++ make-site.sh
@@ -1,6 +1,7 @@
 PAGES="blog.html contact.html index.html projects.html thanks.html"
 BLOG_FILES="src/blog/*.md"
 BLOG_PREFIX="blog-"
+BLOG_TEMPLATE="blog-template.html"
 
 python3 make-blog-index.py "$BLOG_PREFIX" $BLOG_FILES
 
@@ -9,10 +10,10 @@ for p in $PAGES; do
     ./make-page.sh "src/$p" > "http/$p"
 done
 
-./make-page.sh "src/tmp-blog-post.html" > "blog-template.html"
+./make-page.sh "src/tmp-blog-post.html" > "$BLOG_TEMPLATE"
 
 for p in $BLOG_FILES; do
     echo "$p"
     fn=$(basename -s .md $p)
-    $PANDOC --template blog-template.html $p > "http/$BLOG_PREFIX$fn.html"
+    $PANDOC --template "$BLOG_TEMPLATE" $p > "http/$BLOG_PREFIX$fn.html"
 done
