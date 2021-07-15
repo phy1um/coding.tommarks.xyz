da0a33b170eb7bb67ecddae54b14ddc818414567
Tom Marks <tom@tommarks.xyz>
2021-07-15 21:25:36 +1000

    Added some logging stuff to site script

--- make-site.sh
+++ make-site.sh
@@ -3,6 +3,30 @@ BLOG_FILES="src/blog/*.md"
 BLOG_PREFIX="blog-"
 BLOG_TEMPLATE="blog-template.html"
 
+function test_in_path {
+    echo " ------------- "
+    echo "   Locating $1"
+    which $1 > /dev/null 2>&1
+    if [ $? == 0 ]; then
+        echo "Found $1 @ $(which $1)"
+        echo "Version:"
+        $2
+    else
+        echo " /!\\ NOT FOUND /!\\"
+    fi
+
+}
+
+function log_init_status {
+    echo "=== BUILDING SITE ==="
+    echo " DEPLOYING TO: $URL"
+    echo " > source: $REPOSITORY_URL"
+    echo " > branch: $BRANCH"
+    echo " > commit: $COMMIT_REF"
+    test_in_path "python" "python --version"
+    test_in_path "pandoc" "pandoc -v"
+}
+
 function make_page {
     cat "src/tmp-header.html" "$1" "src/tmp-footer.html"
 }
@@ -11,17 +35,30 @@ if [ -z $PANDOC ]; then
     PANDOC="pandoc"
 fi
 
+log_init_status
+
+echo " === Building Blog Index === "
 python3 make-blog-index.py "$BLOG_PREFIX" $BLOG_FILES
 
+echo
+echo " === Making Main Pages === "
+
 for p in $PAGES; do
-    echo "$p"
+    echo "Building page: $p"
     make_page "src/$p" > "http/$p"
 done
+
+echo
+echo " === Making Blog === "
 
 make_page "src/tmp-blog-post.html" > "$BLOG_TEMPLATE"
 
 for p in $BLOG_FILES; do
-    echo "$p"
+    echo "Building blog post: $p"
     fn=$(basename -s .md $p)
     $PANDOC --template "$BLOG_TEMPLATE" $p > "http/$BLOG_PREFIX$fn.html"
 done
+
+echo
+echo
+echo " =========="
